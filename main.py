f783a9551d1fa8f36a0274ed6e6e3d991a0d3a11
BuildTools <unconfigured@null.spigotmc.org>
2020-10-26 23:51:37 -0400

    added Josef

--- main.py
+++ main.py
@@ -5,6 +5,6 @@ def main():
     #Create an instance on your controller object
     pygame.init()
     print("Software Lead is: Emily Greene")
-    print("Backend is: ?")
+    print("Backend is: Josef Schindler")
     print("Frontend is: Roman Raguso")
 main()
